b6a979ef772035a38f7677d9e2e19751182b458b
EB10097 <erykbienmail@gmail.com>
2025-11-16 09:18:56 +0100

    Add files via upload

---
+++ gold_rush_zad2/src/main/java/edu/io/token/GoldToken.java
@@ -0,0 +1,10 @@
+package edu.io.token;
+
+
+public class GoldToken extends Token {
+
+    // stworzenie złota
+    public GoldToken() {
+        super(Label.GOLD_TOKEN_LABEL);
+    }
+}
---
+++ gold_rush_zad2/src/main/java/edu/io/token/Label.java
@@ -0,0 +1,9 @@
+package edu.io.token;
+
+
+public class Label {
+    // Używane symbole na planszy
+        public static final String EMPTY_TOKEN_LABEL = "\u30FB";  // "・"
+        public static final String PLAYER_TOKEN_LABEL = "\uC6C3"; // "웃"
+        public static final String GOLD_TOKEN_LABEL = "\uD83D\uDCB0"; // "💰︎"
+}
