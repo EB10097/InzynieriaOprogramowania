7c3e31240788502b3b93be395f1ad35fdab60f4b
EB10097 <erykbienmail@gmail.com>
2025-12-09 21:39:50 +0100

    zad5

---
+++ src/main/java/edu/io/token/AnvilToken.java
@@ -0,0 +1,8 @@
+package edu.io.token;
+
+public class AnvilToken extends Token {
+    // stworzenie żetonu kowadła
+    public AnvilToken() {
+        super(Label.ANVIL_TOKEN_LABEL);
+    }
+}
---
+++ src/main/java/edu/io/token/Repairable.java
@@ -0,0 +1,5 @@
+package edu.io.token;
+// do napraw
+public interface Repairable {
+    void repair();
+}
